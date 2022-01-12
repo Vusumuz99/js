31cb3f768043d6b2a771fa3d8c8e0c7f1f353aa7
unknown <mpanza.vusi5@gmail.com>
2022-01-12 15:25:03 +0200

    added input

---
+++ script.js
@@ -0,0 +1,10 @@
+
+        
+        var names = [];
+        
+        function myselect() {
+            document.getElementById("team 2").value =
+            document.getElementById("soccer name 1").value;
+           
+        }
+    
