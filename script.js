93574d1cc7cf7708e88f67aecd5ab4769ded8520
unknown <mpanza.vusi5@gmail.com>
2022-01-14 11:10:11 +0200

    added alert

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 
-        
-        var names = [];
-        
-        function myselect() {
-            document.getElementById("team 2").value =
-            document.getElementById("soccer name 1").value;
-           
-        }
-    
+
+var names = [];
+
+function myselect() {
+    document.getElementById("team 2").value =
+        document.getElementById("soccer name 1").value;
+
+
+    if (names == "") {
+        alert("yes can soccer");
+        return;
+    }
+
+}
+
